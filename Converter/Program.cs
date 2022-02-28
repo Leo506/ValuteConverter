25e79338bfdf376dbbdef68f58e6d66bb0979226
Artem <leoartemlev@yandex.ru>
2022-03-01 00:14:11 +0300

    Простой запрос к сайту и получение ответа

--- Converter/Program.cs
+++ Converter/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace Converter
 {
@@ -6,7 +9,42 @@ namespace Converter
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			Task t = Task.Run (RequestAsync);
+			t.Wait ();
+		}
+
+
+
+		private static async Task RequestAsync()
+		{
+			// Адрес ресурса
+			string url = "https://www.cbr-xml-daily.ru/daily_json.js";
+
+			// Создание запроса к сайту
+			HttpWebRequest request = (HttpWebRequest)WebRequest.Create (url);
+
+			// Избавляемся от ошибки недоверия
+			request.ServerCertificateValidationCallback = delegate {
+				return true;
+			};
+
+			// Получение ответа
+			HttpWebResponse response = (HttpWebResponse) await request.GetResponseAsync ();
+
+			// Чтение ответа
+			using (Stream stream = response.GetResponseStream ()) 
+			{
+				using (StreamReader reader = new StreamReader (stream)) 
+				{
+					string line = "";
+					while ((line = reader.ReadLine()) != null) 
+					{
+						Console.WriteLine (line);	
+					}
+				}
+			}
+
+			response.Close ();
 		}
 	}
 }
