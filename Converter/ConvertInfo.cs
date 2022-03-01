17e0fb7a5b9807d13991d72b9f06260b5071fd04
Artem <leoartemlev@yandex.ru>
2022-03-01 20:35:29 +0300

    Добавил структуру, представляющую перевод из одной валюты в другую

---
+++ Converter/ConvertInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Converter
+{
+	public struct ConvertInfo
+	{
+		private string _startValute;  // Исходная валюта
+		public string StartValute 
+		{
+			get { return _startValute; }
+			set
+			{ 
+				if (value.Length != 3)
+					throw new Exception ("Название валюты состоит из 3 букв");
+				_startValute = value;
+			}
+		}
+
+		private string _endValute;  // Конечная валюта
+		public string EndValute
+		{
+			get { return _endValute; }
+			set
+			{
+				if (value.Length != 3)
+					throw new Exception ("Название валюты состоит из 3 букв");
+				_endValute = value;
+			}
+		}
+
+		public float amount { get; set; }
+	}
+}
+
